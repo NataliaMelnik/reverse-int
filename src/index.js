fb2809ce83b447bf5d9c055f382f4aacc3c278af
NataliaMelnik <bukato.natallia@gmail.com>
2021-03-14 21:45:12 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+let reversedString;
+if (n>0){
+    reversedString = String(n).split('').reverse().join('');
+}  else{
+    reversedString = String(n).slice(1).split('').reverse().join('');
 }
+return Number (reversedString);
+}
+
